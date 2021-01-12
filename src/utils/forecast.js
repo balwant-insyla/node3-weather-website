cfc6889f1e93db2c24335da880dbe88fe8c6fb32
Balwant Neeb <balwantneeb@Balwants-MacBook-Air.local>
2021-01-12 15:31:48 +0530

    Added Humidity in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) =>{
          else if(response.body.error){
             callback('Unable to find location !', undefined)
          } else {
-             callback(undefined, 'It is '+ response.body.current.weather_descriptions[0] +'.It is currently '+ response.body.current.temperature +' degree out. It feels like ' +response.body.current.feelslike + ' degree out.')
+             callback(undefined, 'It is '+ response.body.current.weather_descriptions[0] +' and has humidity ' + response.body.current.humidity +' for today. It is currently '+ response.body.current.temperature +' degree out. It feels like ' +response.body.current.feelslike + ' degree out.')
              }
 
     })
